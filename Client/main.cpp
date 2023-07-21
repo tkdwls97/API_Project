821b956c5bdaa5abd07c2c4b038d343ba70ad1e7
tkdwls97 <tkdwlsdl789@gamil.com>
2023-07-22 03:51:25 +0900

    업데이트

--- Client/main.cpp
+++ Client/main.cpp
@@ -10,7 +10,7 @@
 // 전역 변수:
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING]; 
+WCHAR szWindowClass[MAX_LOADSTRING];
            
 ex::Application application;
 
